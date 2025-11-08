e40a0deed78240f44bf497ef5d09fe0da475f543
sharmachirag16062006-svg <sharmachirag16062006@gmail.com>
2025-11-08 22:43:06 +0530

    Update MainLogger.java

--- MainLogger.java
+++ MainLogger.java
@@ -1,30 +1,35 @@
 package studentlogger;
 
 import java.io.IOException;
-import java.util.List;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Arrays;
 
-public class ScoreWriterTask implements Runnable {
-    private final List<StudentRecord> records;
-    private final SafeFileWriter fileWriter;
-    private final int pauseTime;
+public class MainLogger {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Path file = Path.of("student_scores.csv");
 
-    public ScoreWriterTask(List<StudentRecord> records, SafeFileWriter fileWriter, int pauseTime) {
-        this.records = records;
-        this.fileWriter = fileWriter;
-        this.pauseTime = pauseTime;
-    }
+        if (Files.exists(file)) Files.delete(file);
 
-    @Override
-    public void run() {
-        for (StudentRecord record : records) {
-            try {
-                fileWriter.writeEntry(record.toCsvFormat());
-                if (pauseTime > 0) Thread.sleep(pauseTime);
-            } catch (IOException e) {
-                System.err.println("Error during file write: " + e.getMessage());
-            } catch (InterruptedException e) {
-                Thread.currentThread().interrupt();
-            }
-        }
+        SafeFileWriter writer = new SafeFileWriter(file);
+
+        StudentRecord r1 = new StudentRecord("Aarav", "101", 92);
+        StudentRecord r2 = new StudentRecord("Diya", "102", 88);
+        StudentRecord r3 = new StudentRecord("Kiran", "103", 79);
+        StudentRecord r4 = new StudentRecord("Megha", "104", 85);
+
+        ScoreWriterTask task1 = new ScoreWriterTask(Arrays.asList(r1, r2), writer, 40);
+        ScoreWriterTask task2 = new ScoreWriterTask(Arrays.asList(r3, r4), writer, 25);
+
+        Thread t1 = new Thread(task1, "Thread-A");
+        Thread t2 = new Thread(task2, "Thread-B");
+
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+
+        System.out.println("All student submissions completed. File contents:");
+        Files.lines(file).forEach(System.out::println);
     }
 }
